792dbd31eff1fffef19fecbb8766b70030b90cc2
Bryecook <brybcook@gmail.com>
2021-04-20 09:44:03 -0700

    started buttons

--- main-project-folder/frontend/src/index.js
+++ main-project-folder/frontend/src/index.js
@@ -25,4 +25,37 @@ const createLoginForm = () => {
     })
 }
 
+const difficultySelection = () => {
+    div = document.querySelector('#menu')
+    header = document.createElement('h2')
+    header.innerText = "Select difficulty!"
+    header.style.color = "Purple"
+
+    easyButton = document.createElement('button')
+    easyButton.className = "btn-easy"
+    easyButton.innerText = "Easy Quiz"
+    easyButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        easyQuiz()
+    })
+
+    hardButton = document.createElement('button')
+    hardButton.className = "btn-hard"
+    hardButton.innerText = "Hard Quiz"
+    hardButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        hardQuiz()
+    })
+
+    br = document.createElement('br')
+    div.append(header, easyButton, br, hardButton)
+}
+
+const easyQuiz = () => {
+    // fetch array of easy question objects with .filter
+    // select 10 random questions and put into array
+    // forEach thru them giving points to the round if correct answer is select?
+}
+
 menuRender()
+difficultySelection()
