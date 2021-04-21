4b0ea18bf867879477ad0577de38c3fdd03aa597
Cori Blount <heycorihey@icloud.com>
2021-04-21 14:12:43 -0500

    seeds

--- main-project-folder/backend/bikini-bottom-showdown/app/controllers/api/v1/users_controller.rb
+++ main-project-folder/backend/bikini-bottom-showdown/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,11 @@ class Api::V1::UsersController < ApplicationController
         user = User.all
         render json: user
     end
+
+    def create
+        user = User.create(username: user[:username]) 
+          render json: user
+      end
+
+
 end
--- main-project-folder/backend/bikini-bottom-showdown/app/controllers/users_controller.rb
+++ main-project-folder/backend/bikini-bottom-showdown/app/controllers/users_controller.rb
@@ -3,4 +3,32 @@ class UsersController < ApplicationController
         users = User.all 
         render json: users
     end
+
+    def new
+        user = User.new
+        render json: users
+    end
+
+    def create
+        user = User.find_by(user_params) || User.new(user_params)
+        if user.save
+          render json: users
+        else
+          render json: {errors: user.errors}
+        end
+      end
+
+      def edit
+        user = current_user
+      end
+
+      def update
+        user = User.find(params[:id])
+        render json: users
+      end
+    
+    private
+    def user_params
+      params.require(:user).permit(:username)
+    end
 end
